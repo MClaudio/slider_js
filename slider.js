d7c9f93dd2291a206137114b29dfacdf0080d954
Claudio <claum0395@gmail.com>
2019-05-18 19:51:16 -0500

    Se crea el slider con js

---
+++ slider.js
@@ -0,0 +1,39 @@
+let img = [];
+var indice = 0
+
+img[0] = 'img/1.jpg'
+img[1] = 'img/2.jpg'
+img[2] = 'img/3.jpg'
+img[3] = 'img/4.jpg'
+img[4] = 'img/5.jpg'
+img[5] = 'img/6.jpg'
+img[6] = 'img/7.jpg'
+img[7] = 'img/8.jpg'
+img[8] = 'img/9.jpg'
+img[9] = 'img/10.jpg'
+img[10] = 'img/11.jpg'
+
+function cambiarImagen(int) {
+    if (int == 1) {
+        if (indice < img.length - 1) {
+            indice++
+        } else {
+            indice = 0
+        }
+    } else {
+        if (indice > 0) {
+            indice--
+        } else {
+            indice = img.length - 1
+        }
+    }
+    document.getElementById("img").src = img[indice]
+}
+function startSlide() {
+    setInterval(cambiarImagen, 2000)
+}
+function stopSlider() {
+    clearInterval(cambiarImagen);
+}
+
+
